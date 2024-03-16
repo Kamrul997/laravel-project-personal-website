270ac657e65f8b662f04f9bf13bcbfe87127cd70
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2024-03-17 03:40:18 +0600

    forntend design bug solve: menu and contact form shadow problem.

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -6,37 +6,37 @@
         </a>
         <ul class="list-group menu text-center" id="menu">
             <li class="list-group-item">
-                <a class="active" href="{{ route('home.page') }}">
+                <a class="{{ Request::routeIs('home.page') ? 'active' : '' }}" href="{{ route('home.page') }}">
                     <i class="fa fa-home"></i>
                     <span>home</span>
                 </a>
             </li>
             <li class="list-group-item">
-                <a class="{{ (request()->is('/about')) ? 'active' : '' }}" href="{{ route('about.page') }}">
+                <a class="{{ Request::routeIs('about.page') ? 'active' : '' }}" href="{{ route('about.page') }}">
                     <i class="fa fa-user"></i>
                     <span>about</span>
                 </a>
             </li>
             <li class="list-group-item">
-                <a class="{{ (request()->is('/resume')) ? 'active' : '' }}" href="{{ route('resume.page') }}">
+                <a class="{{ Request::routeIs('resume.page') ? 'active' : '' }}" href="{{ route('resume.page') }}">
                     <i class="fa fa-address-book"></i>
                     <span>resume</span>
                 </a>
             </li>
             <li class="list-group-item">
-                <a class="{{ (request()->is('/portfolio')) ? 'active' : '' }}" href="{{ route('portfolio.page') }}">
+                <a class="{{ Request::routeIs('portfolio.page') ? 'active' : '' }}" href="{{ route('portfolio.page') }}">
                     <i class="fa fa-briefcase"></i>
                     <span>works</span>
                 </a>
             </li>
             <li class="list-group-item">
-                <a class="{{ (request()->is('/blog')) ? 'active' : '' }}" href="{{ route('blog.page') }}">
+                <a class="{{ Request::routeIs('blog.page') ? 'active' : '' }}" href="{{ route('blog.page') }}">
                     <i class="fa fa-file-text"></i>
                     <span>blog</span>
                 </a>
             </li>
             <li class="list-group-item">
-                <a class="{{ (request()->is('/contact')) ? 'active' : '' }}" href="{{ route('contact.page') }}">
+                <a class="{{ Request::routeIs('contact.page') ? 'active' : '' }}" href="{{ route('contact.page') }}">
                     <i class="fa fa-envelope-open"></i>
                     <span>contact</span>
                 </a>
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,41 +1,199 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Arshia - Fully Responsive Personal Template</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta name="author" content="Retrina Group" />
 
-        <!--  FavIcon  -->
-        <link rel="shortcut icon" href="assets/img/favicon.ico">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Arshia - Fully Responsive Personal Template</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Retrina Group" />
 
-        <!-- Google Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Muli:300,400,600" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700" rel="stylesheet">
+    <!--  FavIcon  -->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}">
 
-        <!-- Plugin Css -->
-        <link rel="stylesheet" href="assets/css/plugins.css">
-        <!-- Colors Css -->
-        <link rel="stylesheet" href="assets/css/green-color.css" id="option-color">
-        <!--  Custom Style CSS  -->
-        <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/colorfull-main.css">
-    </head>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700" rel="stylesheet">
 
-    <main >
+    <!-- Plugin Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+    <!-- Colors Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/green-color.css') }}" id="option-color">
+    <!--  Custom Style CSS  -->
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/colorfull-main.css') }}">
+</head>
+
+<body data-spy="scroll" data-target="#scrollspy" data-offset="1" class="flat-demo">
+    <!--  Pre Loader  -->
+    <div id="overlayer"></div>
+    <span class="loader"></span>
+    <!-- Portfolio Single Modal Start -->
+    <div class="portfolio-single modal fade" id="portfolio-single" tabindex="-1" role="dialog" aria-labelledby="portfolioModalScrollable" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content text-muted">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="portfolioModalScrollable">My Work</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="pt-4 pb-5">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12 col-md-10 mx-auto">
+                                    <div class="text-center">
+                                        <h2 class="mb-3">Single <span class="base-color"> Portfolio</span></h2>
+                                        <p class="max-width-450">In a professional context it often happens that private clients corder a publication to be made.</p>
+                                    </div>
+                                    <div class="row contact-info mt-5">
+                                        <div class="col-md-4">
+                                            <div class="text-center">
+                                                <div>
+                                                    <h6>Created by:</h6>
+                                                    <p class="mb-0">Jessy Doe</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 p-title-border mt-3 mt-md-0">
+                                            <div class="text-center">
+                                                <div>
+                                                    <h6 class="contact_detail-title">Completed on:</h6>
+                                                    <p class="mb-0">September 30, 2019</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 mt-3 mt-md-0">
+                                            <div class="text-center">
+                                                <div>
+                                                    <h6>Skills:</h6>
+                                                    <p class="mb-0">HTML5 / PHP / CSS3</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="mt-5">
+                                        <div class="portfolio-single-item image-border">
+                                            <img src="assets/img/portfolio-01.jpg" class="d-block" alt="...">
+                                        </div>
+                                    </div>
+                                    <div class="text-md-left mt-5">
+                                        <p class="my-3">These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, typeset text. These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. Only for show.</p>
+                                        <p class="mt-3 mb-0">These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, typeset text. These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. Only for show.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  Portfolio Single Modal End  -->
+
+    <!-- Blog Single Modal Start -->
+    <div class="blog-single modal fade" id="blog-single" tabindex="-1" role="dialog" aria-labelledby="blogModalScrollable" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="blogModalScrollable">Write Your Dreams</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!--   Blog-single   Start -->
+                    <div class="pt-4 pb-5 text-muted">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-10 mx-auto blog">
+                                    <div class="rounded px-lg-5">
+                                        <div class="text-md-left">
+                                            <h4 class="mb-4">The best way to become a good web designer</h4>
+                                            <div class="image-border">
+                                                <img src="assets/img/blog-08.jpg" alt="">
+                                            </div>
+                                            <span class="d-inline-block py-4"> 1 April 2019
+                                        / By <span class="purple-color text-right"> John Doe. </span>
+                                        </span>
+                                            <p>These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, typeset text. These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. Only for show.</p>
+                                            <div class="px-md-5 px-3 py-4 quotation">
+                                                <i class="base-color lni-quotation size-md"></i>
+                                                <h6 class="base-color font-italic mb-0">We Love Buna, lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula felis euismod semper.</h6>
+                                            </div>
+                                            <p class="my-3">These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. This is dummy copy. It is not meant to be read. It has been placed here solely to demonstrate the look and feel of finished, typeset text. These words are here to provide the reader with a basic impression of how actual text will appear in its final presentation. Only for show.</p>
+                                            <ul class="list-unstyled mt-4 checkmark-list">
+                                                <!-- Item-01 -->
+                                                <li class="mb-2">
+                                                    <span class="ml-1">The purpose of lorem ipsum is to create</span>
+                                                </li>
+                                                <!-- Item-02 -->
+                                                <li class="mb-2">
+                                                    <span class="ml-1">In particular, the garbled words of lorem ipsum</span>
+                                                </li>
+                                                <!-- Item-03 -->
+                                                <li class="mb-2">
+                                                    <span class="ml-1">occasionally circumstances occur in which toil</span>
+                                                </li>
+                                            </ul>
+
+                                            <!--Blog Reply-->
+                                            <div class="contact mt-4">
+                                                <div class="contact-form">
+                                                    <h2 class="text-left mb-4">Leave a reply</h2>
+                                                    <form>
+                                                        <div class="row">
+                                                            <div class="col-lg-6 form-item">
+                                                                <div class="form-group">
+                                                                    <input name="name" id="blog-name" type="text" class="form-control" placeholder="Your Name*" required>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-lg-6 form-item">
+                                                                <div class="form-group">
+                                                                    <input name="email" id="blog-email" type="email" class="form-control" placeholder="Your Email*" required>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-12 form-item">
+                                                                <div class="form-group">
+                                                                    <textarea name="comments" id="blog-comments" rows="4" class="form-control" placeholder="Your message..."></textarea>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-12 mt-4 text-left">
+                                                                <div class="button-border">
+                                                                    <button type="submit" class="pill-button" id="blog-submit-btn">Submit</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  Blog Single Modal End  -->
+    <main>
 
         @include('frontend.layouts.header')
 
         @yield('contents')
 
-         <!--   Color Scheme  -->
-         <a class="color-scheme hide text-white bg-base-color d-inline-block" href="javascript:void(0)"><i class="lni-night"></i></a>
-         <a class="color-scheme hide color-scheme-light text-white bg-base-color d-none" href="javascript:void(0)"><i class="lni-sun"></i></a>
+        <!--   Color Scheme  -->
+        {{-- <a class="color-scheme hide text-white bg-base-color d-inline-block" href="javascript:void(0)"><i
+                class="lni-night"></i></a> --}}
+        {{-- <a class="color-scheme hide color-scheme-light text-white bg-base-color d-none" href="javascript:void(0)"><i
+                class="lni-sun"></i></a> --}}
 
-         <!-- Start Color Pallet -->
-         <!-- <div id="color-switcher" class="color-switcher" >
+        <!-- Start Color Pallet -->
+        <!-- <div id="color-switcher" class="color-switcher" >
              <div class="text-center color-pallet hide">
                  <h6 class="text-center mt-3">Select your color</h6>
                  <ul class="pattern">
@@ -72,20 +230,20 @@
                  <a href="#" class="cp-toggle"><i class="fa fa-cog fa-spin"></i></a>
              </div>
          </div> -->
-         <!-- End Color Pallet -->
+        <!-- End Color Pallet -->
 
     </main>
 
     <!--  JavaScripts  -->
- <script data-cfasync="false" src="{{ asset('assets/js/email-decode.min.js') }}">
- </script>
- <script src="{{ asset('assets/js/jquery-3.4.1.min.js') }}"></script>
+    <script data-cfasync="false" src="{{ asset('assets/js/email-decode.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery-3.4.1.min.js') }}"></script>
 
 
- <!--  Plugin Js  -->
- <script src="{{ asset('assets/js/plugins.js') }}"></script>
- <!--  Custom JS  -->
- <script src="{{ asset('assets/js/arshia.js') }}"></script>
+    <!--  Plugin Js  -->
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <!--  Custom JS  -->
+    <script src="{{ asset('assets/js/arshia.js') }}"></script>
 
- </body>
+</body>
+
 </html>
